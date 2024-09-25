d6de7faad51fad1aa94a633f53c9b19c0bb1dceb
Akash Dhok <akashdhok1265@gmail.com>
2024-09-25 12:46:50 +0530

    update

---
+++ day2/app.js
@@ -0,0 +1,16 @@
+function change(){
+    let a = document.querySelector("#img")
+    a.src = "../one.jpg"
+}
+function change2(){
+    let a = document.querySelector("#img")
+    a.src = "../two.jpg"
+}
+function change3(){
+    let a = document.querySelector("#img")
+    a.src = "../three.jpg"
+}
+function change4(){
+    let a = document.querySelector("#img")
+    a.src = "../four.jpg"
+}
---
+++ day2/script.js
@@ -0,0 +1,16 @@
+//change img using js
+
+function change(){
+    let a = document.querySelector("#img")
+    a.src = "1.jpg"
+}
+
+// changing the image on hover on the img 
+function hover(){
+    let a = document.querySelector("#img")
+    a.src = "1.jpg"
+}
+function run(){
+    let a = document.querySelector("#ul")
+    a.type = "circle"
+}
